654cc291daed2cb796022962884bf375dbaf775a
Kazaha-Horii <windyleaveskzh@gmail.com>
2024-04-28 05:11:31 +0000

    feat: add wandb logging

--- train.py
+++ train.py
@@ -1,28 +1,72 @@
+import pandas as pd
 import pytorch_lightning as pl
 import torch
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.loggers import WandbLogger
 
+import wandb
 from data import DataModule
 from model import ColaModel
+
+
+class SamplesVisualisationLogger(pl.Callback):
+    def __init__(self, datamodule):
+        super().__init__()
+
+        self.datamodule = datamodule
+
+    def on_validation_end(self, trainer, pl_module):
+        val_batch = next(iter(self.datamodule.val_dataloader()))
+        sentences = [
+            self.datamodule.tokenizer.decode(batch_item) for batch_item in val_batch["input_ids"]
+        ]
+
+        outputs = pl_module(val_batch["input_ids"], val_batch["attention_mask"])
+        preds = torch.argmax(outputs.logits, 1)
+        labels = val_batch["label"]
+
+        df = pd.DataFrame(
+            {"Sentence": sentences, "Label": labels.numpy(), "Predicted": preds.numpy()}
+        )
+
+        wrong_df = df[df["Label"] != df["Predicted"]]
+        trainer.logger.experiment.log(
+            {
+                "examples": wandb.Table(dataframe=wrong_df, allow_mixed_types=True),
+                "global_step": trainer.global_step,
+            }
+        )
 
 
 def main():
     cola_data = DataModule()
     cola_model = ColaModel()
 
-    checkpoint_callback = ModelCheckpoint(dirpath="./models", monitor="val_loss", mode="min")
-    early_stopping_callback = EarlyStopping(
-        monitor="val_loss", patience=3, verbose=True, mode="min"
+    checkpoint_callback = ModelCheckpoint(
+        dirpath="./models",
+        filename="best-checkpoint.ckpt",
+        monitor="valid/loss",
+        mode="min",
     )
 
+    early_stopping_callback = EarlyStopping(
+        monitor="valid/loss", patience=3, verbose=True, mode="min"
+    )
+
+    wandb_logger = WandbLogger(project="MLOps-Basics")
     trainer = pl.Trainer(
-        default_root_dir="logs",
-        accelerator=("gpu" if torch.cuda.is_available() else "cpu"),
-        max_epochs=5,
-        fast_dev_run=False,
-        logger=pl.loggers.TensorBoardLogger("logs/", name="cola", version=1),
-        callbacks=[checkpoint_callback, early_stopping_callback],
+        max_epochs=1,
+        logger=wandb_logger,
+        callbacks=[
+            checkpoint_callback,
+            SamplesVisualisationLogger(cola_data),
+            early_stopping_callback,
+        ],
+        log_every_n_steps=10,
+        deterministic=True,
+        # limit_train_batches=0.25,
+        # limit_val_batches=0.25
     )
     trainer.fit(cola_model, cola_data)
 
